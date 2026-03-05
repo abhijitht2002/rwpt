b097eb1fe8d21fd364fc75f5e9703851405b7c48
abhijith <abhijitht210@gmail.com>
2026-03-05 20:53:36 +0530

    backend routes typo

--- backend/routes/tasks.routes.js
+++ backend/routes/tasks.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { listTasks } = require("../controllers/tasks.controller");
-const { protect } = require("../middlewares/auth.middleware");
+const { protect, role } = require("../middlewares/auth.middleware");
 
 router.get("/:filter", protect, role("MANAGER", "EMPLOYEE"), listTasks);
 
